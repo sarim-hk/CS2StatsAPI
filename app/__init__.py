4616b62a3f113e1c1b57e330df397affd13594f4
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-12-12 16:49:04 +0000

    add scheduler to set player of the week

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, g
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+from .jobs.set_player_of_the_week import set_player_of_the_week
 import mysql.connector
 import json
 import os
@@ -7,15 +9,18 @@ import os
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
+    # Load configuration
     config_path = os.path.join(app.instance_path, "config.json")
     with open(config_path) as config_file:
         app.config.update(json.load(config_file))
 
     CORS(app)
 
+    # Register blueprint
     from app.routes import bp as main_bp
     app.register_blueprint(main_bp)
 
+    # Database connection
     def get_db_connection():
         if "db" not in g:
             g.db = mysql.connector.connect(
@@ -28,12 +33,26 @@ def create_app():
 
     @app.before_request
     def before_request():
-        g.db = get_db_connection()
+        g.db = get_db_connection(g)
 
     @app.teardown_request
     def teardown_request(exception):
         db = g.pop("db", None)
         if db is not None:
             db.close()
+
+    scheduler = BackgroundScheduler()
+
+    def start_scheduler():
+        scheduler.add_job(func=set_player_of_the_week, trigger="cron", day_of_week="mon", hour=0, minute=0, args=[app, get_db_connection])
+        scheduler.start()
+
+    # Start the scheduler
+    start_scheduler()
+
+    # Graceful shutdown of the scheduler
+    @app.teardown_appcontext
+    def shutdown_scheduler(exception=None):
+        scheduler.shutdown(wait=False)
 
     return app
---
+++ app/jobs/set_player_of_the_week.py
@@ -0,0 +1,165 @@
+from flask import g
+import traceback
+from ..routes.playerstats_panel import get_stats, get_match_results_date_range, get_split_round_ids_from_match_ids, combine_stats
+
+def set_player_of_the_week(app, get_db_connection):
+    with app.app_context():
+        db = get_db_connection()
+        cursor = db.cursor()
+
+        try:
+            cursor = g.db.cursor(dictionary=True)
+            all_player_stats_7_days = get_all_players_stats_last_7_days(cursor)
+            all_player_stats_overall = get_all_players_stats_overall(cursor)
+
+            cursor.execute("UPDATE CS2S_PlayerInfo SET PlayerOfTheWeek = 0")
+
+            # Calculate rating increase
+            player_rating_increases = {}
+            for player_id, seven_day_stats in all_player_stats_7_days.items():
+
+                if player_id in all_player_stats_overall:
+                    seven_day_rating = seven_day_stats.get("Rating", 0)
+                    overall_rating = all_player_stats_overall[player_id].get("Rating", 0)
+                    
+                    rating_increase = seven_day_rating - overall_rating
+                    player_rating_increases[player_id] = {
+                        "seven_day_rating": seven_day_rating,
+                        "overall_rating": overall_rating,
+                        "rating_increase": rating_increase
+                    }
+
+            # Find top 3 rating increases
+            if player_rating_increases:
+                top_3_players = sorted(
+                    player_rating_increases.items(), 
+                    key=lambda x: x[1]["rating_increase"], 
+                    reverse=True
+                )[:3]
+
+                # Update PlayerOfTheWeek in DB
+                for idx, (player_id, _) in enumerate(top_3_players, start=1):
+                    cursor.execute(
+                        "UPDATE CS2S_PlayerInfo SET PlayerOfTheWeek = %s WHERE PlayerID = %s", 
+                        (idx, player_id)
+                    )
+                    
+                db.commit()
+
+            return player_rating_increases
+
+        except Exception as e:
+            error_traceback = traceback.format_exc()
+            print(error_traceback)
+            db.rollback()
+
+def get_all_players_stats_last_7_days(cursor):
+    cursor.execute("""
+    SELECT DISTINCT PlayerID
+    FROM CS2S_Player_Matches pm
+    JOIN CS2S_Match m ON pm.MatchID = m.MatchID
+    WHERE m.MatchDate >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+    """)
+    
+    player_ids = [row["PlayerID"] for row in cursor.fetchall()]
+    
+    if not player_ids:
+        return {}
+    
+    all_player_stats = {}
+
+    for player_id in player_ids:
+        results = get_match_results_date_range(cursor, "7days", [player_id])
+
+        # Skip players with less than 3 matches
+        if not results or len(set(result["MatchID"] for result in results)) < 3:
+            continue
+
+        # Extract match and round IDs
+        match_ids = list(set(result["MatchID"] for result in results))
+        matches_won = sum(1 for result in results if result["Result"] == "Win")
+        matches_played = len(match_ids)
+        t_round_ids, ct_round_ids = get_split_round_ids_from_match_ids(cursor, match_ids, player_id)
+        
+        t_stats = get_stats(cursor, t_round_ids, player_id)
+        ct_stats = get_stats(cursor, ct_round_ids, player_id)
+        combined_stats = combine_stats(t_stats, ct_stats)
+
+        all_player_stats[player_id] = {
+            "Overall": combined_stats or 0,
+            "Terrorist": t_stats or 0,
+            "CounterTerrorist": ct_stats or 0,
+            "MatchesPlayed": matches_played or 0,
+            "MatchesWon": matches_won or 0,
+            "MatchIDs": match_ids or [],
+            "Rating": combined_stats["Rating"] if combined_stats else 0
+        }
+
+    return all_player_stats
+
+def get_all_players_stats_overall(cursor):
+    cursor.execute("""
+    SELECT 
+        PlayerID,
+        SUM(Kills) AS TotalKills,
+        SUM(Headshots) AS TotalHeadshots,
+        SUM(Assists) AS TotalAssists,
+        SUM(Deaths) AS TotalDeaths,
+        SUM(Damage) AS TotalDamage,
+        SUM(UtilityDamage) AS TotalUtilityDamage,
+        SUM(EnemiesFlashed) AS TotalEnemiesFlashed,
+        SUM(GrenadesThrown) AS TotalGrenadesThrown,
+        SUM(ClutchAttempts) AS TotalClutchAttempts,
+        SUM(ClutchWins) AS TotalClutchWins,
+        SUM(DuelAttempts) AS TotalDuelAttempts,
+        SUM(DuelWins) AS TotalDuelWins,
+        SUM(RoundsKAST) AS TotalRoundsKAST,
+        SUM(RoundsPlayed) AS TotalRoundsPlayed
+    FROM CS2S_PlayerStats
+    GROUP BY PlayerID
+    HAVING SUM(RoundsPlayed) > 0
+    """)
+    
+    all_player_stats = {}
+    
+    for row in cursor.fetchall():
+        player_id = row["PlayerID"]
+        total_rounds_played = row["TotalRoundsPlayed"]
+        
+        stats = {
+            "PlayerID": player_id,
+            "Damage": row["TotalDamage"],
+            "UtilityDamage": row["TotalUtilityDamage"],
+            "Kills": row["TotalKills"],
+            "Assists": row["TotalAssists"],
+            "Deaths": row["TotalDeaths"],
+            "Headshots": row["TotalHeadshots"],
+            "Blinds": {
+                "Count": row["TotalEnemiesFlashed"],
+                "TotalDuration": 0
+            },
+            "Clutches": {
+                "Attempts": row["TotalClutchAttempts"],
+                "Wins": row["TotalClutchWins"]
+            },
+            "Duels": {
+                "Attempts": row["TotalDuelAttempts"],
+                "Wins": row["TotalDuelWins"]
+            },
+            "GrenadesThrown": row["TotalGrenadesThrown"],
+            "RoundsPlayed": total_rounds_played,
+            "RoundsKAST": row["TotalRoundsKAST"]
+        }
+
+        stats["KAST"] = round(((stats["RoundsKAST"] / total_rounds_played) * 100), 2) if total_rounds_played > 0 else 0
+        stats["ADR"] = round(stats["Damage"] / total_rounds_played, 2) if total_rounds_played > 0 else 0
+        stats["KPR"] = round(stats["Kills"] / total_rounds_played, 2) if total_rounds_played > 0 else 0
+        stats["APR"] = round(stats["Assists"] / total_rounds_played, 2) if total_rounds_played > 0 else 0
+        stats["DPR"] = round(stats["Deaths"] / total_rounds_played, 2) if total_rounds_played > 0 else 0
+
+        stats["Impact"] = round(2.13 * float(stats["KPR"]) + 0.42 * (float(stats["Assists"]) / float(total_rounds_played)) - 0.41, 2) if total_rounds_played > 0 else 0
+        stats["Rating"] = round((0.0073 * float(stats["KAST"]) + 0.3591 * float(stats["KPR"]) + -0.5329 * float(stats["DPR"]) + 0.2372 * float(stats["Impact"]) + 0.0032 * float(stats["ADR"]) + 0.1587), 2) if total_rounds_played > 0 else 0
+
+        all_player_stats[player_id] = stats
+
+    return all_player_stats

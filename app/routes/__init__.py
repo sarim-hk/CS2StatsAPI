24f8f9d257f49adc70385cbdbf99079aa0ca776e
ethanm123 <ethanmansbridge1@gmail.com>
2025-02-15 16:13:02 +0000

    added opening rates endpoint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,13 +1,15 @@
 from flask import Blueprint
-from .players_panel import players_panel_bp
-from .player_panel import player_panel_bp
-from .matches_panel import matches_panel_bp
-from .match_panel import match_panel_bp
-from .live_match_panel import live_match_panel_bp
-from .playerstats_panel import playerstats_panel_bp
-from .playerelo_panel import playerelo_panel_bp
 
-bp = Blueprint('main', __name__)
+from .live_match_panel import live_match_panel_bp
+from .match_panel import match_panel_bp
+from .matches_panel import matches_panel_bp
+from .opening_rates_panel import opening_rates_bp
+from .player_panel import player_panel_bp
+from .playerelo_panel import playerelo_panel_bp
+from .players_panel import players_panel_bp
+from .playerstats_panel import playerstats_panel_bp
+
+bp = Blueprint("main", __name__)
 
 bp.register_blueprint(players_panel_bp)
 bp.register_blueprint(player_panel_bp)
@@ -16,4 +18,4 @@ bp.register_blueprint(match_panel_bp)
 bp.register_blueprint(live_match_panel_bp)
 bp.register_blueprint(playerstats_panel_bp)
 bp.register_blueprint(playerelo_panel_bp)
-
+bp.register_blueprint(opening_rates_bp)

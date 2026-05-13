d606e907571b315cdb322a8d81fe5b9a9db85010
sarim-hk <shktv03@gmail.com>
2026-05-13 02:33:22 +0100

    remove live match panel

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 
-from .live_match_panel import router as live_match_router
 from .match_panel import router as match_router
 from .matches_panel import router as matches_router
 from .player_panel import router as player_router
@@ -14,6 +13,5 @@ router.include_router(players_router)
 router.include_router(player_router)
 router.include_router(matches_router)
 router.include_router(match_router)
-router.include_router(live_match_router)
 router.include_router(playerstats_router)
 router.include_router(playerelo_router)

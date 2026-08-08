3a8cf3951d729b3603d2b7338c6828d9afe4dfce
sarim-hk <shktv03@gmail.com>
2026-08-09 00:25:11 +0100

    styling

--- app/routes/matches_panel.py
+++ app/routes/matches_panel.py
@@ -73,7 +73,6 @@ def fetch_matches(db, player_id=None, map_name=None, page=None):
         if cursor:
             cursor.close()
 
-
 @router.get("/matches_panel")
 def matches_panel(player_id = None, map_name = Query(None, alias="map"), page = Query(None, ge=1), db=Depends(get_db)):
     return fetch_matches(db=db, player_id=player_id, map_name=map_name, page=page)
